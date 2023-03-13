61337751af711051df7626026bcbdb8a944737eb
Kristijan Imprić <kiki.impric@gmail.com>
2023-03-13 21:03:15 +0100

    Title and des changed

--- react/src/pages/components/Hero.tsx
+++ react/src/pages/components/Hero.tsx
@@ -12,11 +12,7 @@ function Hero() {
                 Delve into the world of Kratos and uncover the secrets of the
                 pre-Nordic era God of War games. From battles against the gods
                 of Olympus to encounters with the Titans, this website is the
-                ultimate resource for fans of the series. Explore rich lore,
-                intricate details, and epic battles that defined Kratos'
-                journey. With comprehensive information, in-depth analysis, and
-                exclusive insights, The God of War Wiki is the definitive guide
-                to the iconic video game series.
+                ultimate resource for fans of the series.
             </p>
         </div>
     );
