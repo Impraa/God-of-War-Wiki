ec6a64b5389fb5637e2274829ba61763b09e79a3
Kristijan Imprić <kiki.impric@gmail.com>
2023-03-27 19:15:29 +0200

    Registered and home changed a bit

--- react/src/pages/components/Hero.tsx
+++ react/src/pages/components/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
             <h2 className="title">
                 Chronicles of the God of War: The Epic Journey through Ancient
                 Mythology
-            </h2>{" "}
+            </h2>
             <p className="description">
                 Delve into the world of Kratos and uncover the secrets of the
                 pre-Nordic era God of War games. From battles against the gods
