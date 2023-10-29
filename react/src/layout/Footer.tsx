28cff50ec0419d23b72848166eaadc974315937b
Kristijan Imprić <kiki.impric@gmail.com>
2023-10-29 19:26:50 +0100

    restarted the whole project

--- react/src/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/layout/Footer.scss";
-
-function Footer() {
-    return <div className="footer">&copy; God of War Wiki 2023</div>;
-}
-
-export default Footer;
--- react/src/layout/components/NavTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NavTitle() {
-    return <div className="nav-title">God of War Wiki</div>;
-}
-
-export default NavTitle;
