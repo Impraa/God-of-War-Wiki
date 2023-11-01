7a49782b9e849806487f4b118479abc1e3357dfb
Kristijan Imprić <kiki.impric@gmail.com>
2023-11-01 23:38:55 +0100

    Added functions for post delete,update,get one/all

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -34,5 +35,75 @@ class PostController extends Controller
         }
 
         return response()->json(["message" => "Post was successfully created"], 201);
+    }
+
+    public function getOne(Request $request)
+    {
+        $data = $request->validate([
+            "post_id" => ["required", "string"],
+        ]);
+
+        $post = Post::find($data["post_id"]);
+
+        if (Post::isEmpty($post)) {
+            return response()->json(["message" => "There is no such post"], 404);
+        }
+
+        return response()->json(["message" => "Post was successfully found", "post" => $post], 200);
+    }
+
+    public function getAll()
+    {
+        $posts = Post::all();
+        if (Post::isEmpty($posts)) {
+            return response()->json(["message" => "No posts where found"], 404);
+        }
+        return response()->json(["message" => "All posts recived", "post" => $posts], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            "post_id" => ["required", "string"],
+            "title" => ["string"],
+            'description' => ['string'],
+            "image" => ['image', 'max:3000'],
+
+        ]);
+
+        $post = Post::find($id);
+        if (Post::isEmpty($post)) {
+            return response()->json(["message" => "No post was found"], 404);
+        }
+        $post->name = $request->name;
+        $post->description = $request->description;
+
+        if ($post->image && $data["image"] != $post->image) {
+            $file = $request->file("image");
+
+            $extension = $file->getClientOriginalExtension();
+
+            $filename = $post->id . '-' . uniqid() . $extension;
+
+            $path = $file->storeAs('public/postImages', $filename);
+
+            Storage::delete('' . $post->image);
+
+            $post->image = $path;
+        }
+
+        $post->save();
+
+        return response()->json(['message' => 'Post was successfully updated', "post" => $post], 200);
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        if (Post::isEmpty($post)) {
+            return response()->json(["message" => "Post was not found"], 404);
+        }
+        $post->delete();
+        return response()->json(["message" => "Post was deleted successfully", "post" => $post], 200);
     }
 }
